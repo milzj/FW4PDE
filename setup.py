0667c1cc09f56c92e2a46bf68362d0ebd0f6d6a4
Johannes Milz <milz@ma.tum.de>
2023-01-28 11:56:19 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='fw4pde',
+      version='1.0.0',
+      description='Frank--Wolfe algorithms for PDE-constrained optimization',
+      author="Johannes Milz",
+      author_email='johannes.milz@gatech.edu',
+      url='https://github.com/milzj/FW4PDE',
+      packages=find_packages(),
+      install_requires=[
+        "numpy",
+        "scipy"
+      ]
+)
